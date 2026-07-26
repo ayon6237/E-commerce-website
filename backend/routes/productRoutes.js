778a9f7d2c1204b7547f0f32c032f361f852239b
Ayon Banerjee <ayon6237@gmail.com>
2026-07-27 00:35:48 +0600

    user review api

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getProducts, createProduct, getProductById, updateProduct, deleteProduct } =require('../controllers/productController.js') ;
+const { getProducts, createProduct, getProductById, updateProduct, deleteProduct, createProductReview } =require('../controllers/productController.js') ;
 const { protect, admin } = require('../middlewares/authMiddleware.js');
 
 const router = express.Router();
@@ -12,5 +12,7 @@ router.route('/:id')
   .get(getProductById)
   .put(protect, admin, updateProduct)
   .delete(protect, admin, deleteProduct);
+
+router.route('/:id/reviews').post(protect, createProductReview);
 
 module.exports = router

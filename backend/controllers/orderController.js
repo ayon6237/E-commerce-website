3573cfd901af65d48039ea0f9f473ad058043c3a
Ayon Banerjee <ayon6237@gmail.com>
2026-07-27 00:13:58 +0600

    updated

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -36,4 +36,31 @@ const addOrderItems = async (req, res) => {
   }
 };
 
-module.exports = { addOrderItems };
+
+const getOrderById = async (req, res) => {
+  try {
+  
+    const order = await Order.findById(req.params.id).populate('user', 'name email');
+
+    if (order) {
+      res.json(order);
+    } else {
+      res.status(404).json({ message: 'Order not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error', error: error.message });
+  }
+};
+
+
+const getMyOrders = async (req, res) => {
+  try {
+    
+    const orders = await Order.find({ user: req.user._id });
+    res.json(orders);
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error', error: error.message });
+  }
+};
+
+module.exports = { addOrderItems ,getOrderById , getMyOrders};
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { addOrderItems } = require('../controllers/orderController.js');
+const { addOrderItems, getMyOrders, getOrderById } = require('../controllers/orderController.js');
 const { protect } = require('../middlewares/authMiddleware.js');
 
 const router = express.Router();
 
 
 router.post('/', protect, addOrderItems);
+router.get('/myorders', protect, getMyOrders);
+router.get('/:id', protect, getOrderById);
 
 module.exports = router;

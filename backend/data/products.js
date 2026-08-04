b493c2276a9ee2fc01ff018a17565f128016a17d
Ayon Banerjee <ayon6237@gmail.com>
2026-08-04 23:52:05 +0600

    backend cpmpleted

---
+++ backend/data/products.js
@@ -0,0 +1,37 @@
+const products = [
+  {
+    name: 'Airpods Wireless Bluetooth Headphones',
+    image: '/images/airpods.jpg',
+    description: 'Bluetooth technology lets you connect it with compatible devices wirelessly.',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: 'iPhone 11 Pro 256GB Memory',
+    image: '/images/phone.jpg',
+    description: 'Introducing the iPhone 11 Pro. A transformative triple-camera system.',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 599.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: 'Sony Playstation 4 Pro White Version',
+    image: '/images/playstation.jpg',
+    description: 'The ultimate home entertainment center starts with PlayStation.',
+    brand: 'Sony',
+    category: 'Electronics',
+    price: 399.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  }
+];
+
+module.exports = products;
